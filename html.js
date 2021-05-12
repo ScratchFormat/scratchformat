72a909b0b9e17aa2dbc82bb48891a920b2c52e2a
Daniel <brikbusters@gmail.com>
2021-05-12 18:05:33 -0400

    HTML info

--- html.js
+++ html.js
@@ -2,4 +2,6 @@ var helpMsg = `
 <a href="https://github.com/ScratchFormat/ScratchFormat2/issues" style="color: #12b1e4;">Report issues at our Github</a>
 If you do not own a Github account, simply comment on my profile
 <a href="https://scratch.mit.edu/users/pufflegamerz/" style="color: #12b1e4;">@pufflegamerz</a>
+<p>!! ScratchFormat is not garunteed to work perfectly alongside other Scratch extensions.</p>
+<p>!! Remember <a href="https://scratch.mit.edu/discuss/topic/284272/" style="color: #12b1e4;">Scratch Browser extension policy</a></p>
 `;
